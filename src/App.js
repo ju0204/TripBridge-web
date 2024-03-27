88305bff9ec33eed8d46c9ef0f0832178b285029
Juhee Baek <p1106000@gmail.com>
2024-03-27 14:28:08 +0900

    feat: nav기능

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignUp from './pages/user/signup/signup';
+import Nav from './components/header/nav'
+import LogIn from './pages/user/login/login';
+import PasswordCheck from './pages/user/password/pwsearch';
+import Main from './pages/main/main'; // 메인 페이지 컴포넌트 import
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 로그아웃 처리 함수
+  const handleLogout = () => {
+    // 여기에 로그아웃 처리 로직을 작성합니다.
+    setIsLoggedIn(false); // 예시로 상태만 변경하는 코드를 작성
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Routes> {/* Switch 대신에 Routes를 사용 */}
+        <Route path="/" element={<Main />} /> {/* "/" 경로에 Main 컴포넌트를 연결 */}
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* 다른 페이지들의 라우팅 설정도 필요 */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/main/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css'; // 분리한 CSS 파일을 불러옵니다.
+
+const Main = () => {
+  return (
+    <div className="wrapper">
+      <p>메인페이지 구성</p>
+    </div>
+  );
+};
+
+export default Main;
