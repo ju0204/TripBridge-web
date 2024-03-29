ad07fba5294344a2b093073a0442244800fda323
Juhee Baek <p1106000@gmail.com>
2024-03-29 21:04:39 +0900

    feat: 게시판 dropbar 추가

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './nav.css'; 
+import './nav.css';
 
 const Nav = ({ isLoggedIn, username, onLogout }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowDropdown(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowDropdown(false);
+  };
+
+  const handleDropdownMouseEnter = () => {
+    setShowDropdown(true);
+  };
+
+  const handleDropdownMouseLeave = () => {
+    setShowDropdown(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-container">
@@ -10,7 +28,17 @@ const Nav = ({ isLoggedIn, username, onLogout }) => {
           <li className="nav-item"><Link className="styled-link" to="/">홈</Link></li>
           <li className="nav-item"><Link className="styled-link" to="/about">여행지 추천</Link></li>
           <li className="nav-item"><Link className="styled-link" to="/contact">동선짜기</Link></li>
-          <li className="nav-item"><Link className="styled-link" to="/contact">게시판</Link></li>
+          <li className="nav-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="styled-link" onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}>
+              게시판
+            </div>
+            {showDropdown && (
+              <div className="dropdown-item" onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}>
+                <Link to="/board">여행 게시판</Link>
+                <Link to="/mate-board">여행 메이트 구하기</Link>
+              </div>
+            )}
+          </li>
           {isLoggedIn && (
             <li className="nav-item">{username}님 환영합니다.</li>
           )}
