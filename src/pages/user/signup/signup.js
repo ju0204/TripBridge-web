6fb77d430a20bc94f90a1a4a10a32743c24a78ce
Juhee Baek <p1106000@gmail.com>
2024-04-08 14:26:28 +0900

    fix: 회원가입 중복 수정

--- src/pages/user/signup/signup.js
+++ src/pages/user/signup/signup.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import './signup.css';
 import { signup } from '../../../api/user';
@@ -13,6 +12,8 @@ const SignUp = () => {
     confirmPassword: ''
   });
   const [errors, setErrors] = useState({});
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -40,10 +41,25 @@ const SignUp = () => {
           alarm: '1',
           alarm2: '1'
         });
-        console.log('회원가입 성공:', response);
-        navigate('/login');
+        console.log('회원가입 결과:', response);
+        if (response.result === true) {
+          console.log('회원가입 성공!');
+          navigate('/login');
+        } else {
+          if (response.message === "중복된 Email 입니다.") {
+            console.log('이메일 중복');
+            setPopupMessage('중복된 이메일입니다!');
+            setShowPopup(true);
+          } else if (response.message === "중복된 Nickname 입니다.") {
+            console.log('닉네임 중복');
+            setPopupMessage('중복된 닉네임입니다!');
+            setShowPopup(true);
+          }
+        }
       } catch (error) {
         console.error('회원가입 실패:', error);
+        setShowPopup(true);
+        setPopupMessage('서버 오류가 발생했습니다.');
       }
     } else {
       setErrors(validationErrors);
@@ -83,7 +99,9 @@ const SignUp = () => {
     return errors;
   };
 
-  
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="sign-up-page">
@@ -92,26 +110,25 @@ const SignUp = () => {
         <form className="sign-up-form" onSubmit={handleSubmit}>
           <label className="styled-label">이름</label>
           <input
-          className={`styled-input ${errors.name && 'error'}`}
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
+            className={`styled-input ${errors.name && 'error'}`}
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
           />
           {errors.name && <span className="styled-error">{errors.name}</span>}
           <label className="styled-label">닉네임</label>
           <input
-          className={`styled-input ${errors.nickname && 'error'}`}
-          type="text"
-          name="nickname"
-          value={formData.nickname}
-          onChange={handleChange}
-          required
+            className={`styled-input ${errors.nickname && 'error'}`}
+            type="text"
+            name="nickname"
+            value={formData.nickname}
+            onChange={handleChange}
+            required
           />
           {errors.nickname && <span className="styled-error">{errors.nickname}</span>}
           <label className="styled-label">이메일 주소</label>
-        
           <input
             className={`styled-input ${errors.email && 'error'}`}
             type="email"
@@ -120,91 +137,84 @@ const SignUp = () => {
             onChange={handleChange}
             required
           />
-          
           {errors.email && <span className="styled-error-email">{errors.email}</span>}
-
           <label className="styled-label">비밀번호</label>
           <input
-          className={`styled-input ${errors.password && 'error'}`}
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
+            className={`styled-input ${errors.password && 'error'}`}
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
           />
           {errors.password && <span className="styled-error">{errors.password}</span>}
-        
           <label className="styled-label">비밀번호 확인</label>
           <input
-          className={`styled-input ${errors.confirmPassword && 'error'}`}
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
+            className={`styled-input ${errors.confirmPassword && 'error'}`}
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
           />
           {errors.confirmPassword && <span className="styled-error">{errors.confirmPassword}</span>}
-        
-          <br/>
-          <br/>
-
-          
+          <br />
+          <br />
           {/* 약관동의 추가 */}
           <label className="agreement-label">약관 동의</label>
           <div class="contents">
             <ul class="terms__list">
               <li class="terms__box">
-              <div class="input__check">
-                <input type="checkbox" class="required"/>
-                <label for="termsOfService">Trip Bridge 이용약관 동의</label>
-              </div>
-              <div class="terms__content">
-                여러분을 환영합니다. Trip Bridge 서비스 및 제품(이하 ‘서비스’)을 이용해
-                주셔서 감사합니다. 본 약관은 다양한 JUN 서비스의 이용과 관련하여
-                Trip Bridge 서비스를 제공하는 Trip Bridge 주식회사(이하 ‘Trip Bridge’)와 이를 이용하는
-                Trip Bridge 서비스 회원(이하 ‘회원’) 또는 비회원과의 관계를 설명하며,
-                아울러 여러분의 Trip Bridge 서비스 이용에 도움이 될 수 있는 유익한
-                정보를 포함하고 있습니다. Trip Bridge 서비스를 이용하시거나 Trip Bridge 서비스
-                회원으로 가입하실 경우 여러분은 본 약관 및 관련 운영 정책을
-                확인하거나 동의하게 되므로, 잠시 시간을 내시어 주의 깊게 살펴봐
-                주시기 바랍니다.
-              </div>
+                <div class="input__check">
+                  <input type="checkbox" class="required" />
+                  <label for="termsOfService">Trip Bridge 이용약관 동의</label>
+                </div>
+                <div class="terms__content">
+                  여러분을 환영합니다. Trip Bridge 서비스 및 제품(이하 ‘서비스’)을 이용해 주셔서 감사합니다. 본 약관은
+                  다양한 JUN 서비스의 이용과 관련하여 Trip Bridge 서비스를 제공하는 Trip Bridge 주식회사(이하 ‘Trip
+                  Bridge’)와 이를 이용하는 Trip Bridge 서비스 회원(이하 ‘회원’) 또는 비회원과의 관계를 설명하며,
+                  아울러 여러분의 Trip Bridge 서비스 이용에 도움이 될 수 있는 유익한 정보를 포함하고 있습니다.
+                  Trip Bridge 서비스를 이용하시거나 Trip Bridge 서비스 회원으로 가입하실 경우 여러분은 본 약관 및 관련
+                  운영 정책을 확인하거나 동의하게 되므로, 잠시 시간을 내시어 주의 깊게 살펴봐 주시기 바랍니다.
+                </div>
               </li>
               <li class="terms__box">
-              <div class="input__check">
-                <input type="checkbox" class="required"/>
-               <label for="termsOfService">개인정보 수집 및 이용 동의</label>
-               </div>
-              <div class="terms__content">
-                개인정보보호법에 따라 Trip Bridge에 회원가입 신청하시는 분께 수집하는
-                개인정보의 항목, 개인정보의 수집 및 이용목적, 개인정보의 보유 및
-                이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내
-                드리오니 자세히 읽은 후 동의하여 주시기 바랍니다.
-                1. 수집하는 개인정보 이용자는 회원가입을 하지 않아도 정보 검색, 뉴스 보기 등
-                대부분의 네이버 서비스를 회원과 동일하게 이용할 수 있습니다.
-                2. 이용자가 메일, 캘린더, 카페, 블로그 등과 같이 개인화 혹은 회원제
-                서비스를 이용하기 위해 회원가입을 할 경우, 네이버는 서비스
-                이용을 위해 필요한 최소한의 개인정보를 수집합니다.
+                <div class="input__check">
+                  <input type="checkbox" class="required" />
+                  <label for="termsOfService">개인정보 수집 및 이용 동의</label>
+                </div>
+                <div class="terms__content">
+                  개인정보보호법에 따라 Trip Bridge에 회원가입 신청하시는 분께 수집하는 개인정보의 항목, 개인정보의
+                  수집 및 이용목적, 개인정보의 보유 및 이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내
+                  드리오니 자세히 읽은 후 동의하여 주시기 바랍니다. 1. 수집하는 개인정보 이용자는 회원가입을 하지 않아도
+                  정보 검색, 뉴스 보기 등 대부분의 네이버 서비스를 회원과 동일하게 이용할 수 있습니다. 2. 이용자가
+                  메일, 캘린더, 카페, 블로그 등과 같이 개인화 혹은 회원제 서비스를 이용하기 위해 회원가입을 할 경우,
+                  네이버는 서비스 이용을 위해 필요한 최소한의 개인정보를 수집합니다.
+                </div>
+              </li>
+            </ul>
+          </div>
+          {/* Checkbox error message */}
+          {errors.confirmCheckbox && <span className="styled-error-agree">{errors.confirmCheckbox}</span>}
+          <div className="styled-button-container">
+            <button className="styled-button" type="submit">
+              회원가입
+            </button>
+          </div>
+        </form>
+        {showPopup && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <div className="popup-content">
+                <p>{popupMessage}</p>
+                <button onClick={handleClosePopup}>확인</button>
               </div>
-            </li>
-          </ul>
-        </div>
-
-        {/* Checkbox error message */}
-        {errors.confirmCheckbox && <span className="styled-error-agree">약관에 동의해야 합니다.</span>}
-
-
-        <div className="styled-button-container">
-            <button className="styled-button" type="submit">회원가입</button>
-        </div>
-      </form>
-    
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default SignUp;
-
-
-
