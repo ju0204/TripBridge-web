b91a9d3fbe40b1990e841fc2d88f5a4d64023ae9
Juhee Baek <p1106000@gmail.com>
2024-04-18 17:36:26 +0900

    feat: 필터 메인화면 추가, 로그인 유지

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,3 +1,4 @@
+// Nav.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
@@ -25,9 +26,9 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
     <nav className="navbar">
       <div className="nav-container">
         <ul className="nav-list-1">
-        <li className="nav-item"><div className="styled-trip">Trip-Bridge</div></li>
+          <li className="nav-item"><div className="styled-trip">Trip-Bridge</div></li>
           <li className="nav-item"><Link className="styled-link" to="/">홈</Link></li>
-          <li className="nav-item"><Link className="styled-link" to="/about">여행지 추천</Link></li>
+          <li className="nav-item"><Link className="styled-link" to="/filter">여행지 추천</Link></li>
           <li className="nav-item"><Link className="styled-link" to="/contact">동선추천</Link></li>
           <li className="nav-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className="styled-link" onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}>
@@ -40,10 +41,9 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
               </div>
             )}
           </li>
-          
         </ul>
         <ul className="nav-list-2">
-          {!isLoggedIn && (
+          {!isLoggedIn && ( // Render login and signup links only if not logged in
             <>
               <li className="auth-item"><Link className="styled-link" to="/login">로그인</Link></li>
               <li className="auth-item"><Link className="styled-link" to="/signup">회원가입</Link></li>
@@ -52,9 +52,9 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
           {isLoggedIn && (
             <>
               <li className="nav-item">
-                <div className="styled-user">{nickname}님 환영합니다.</div></li>
-              <li className="nav-item"><Link className="styled-link" onClick={onLogout}>로그아웃</Link></li>
-              
+                <div className="styled-user">{nickname}님 환영합니다.</div>
+              </li>
+              <li className="nav-item"><Link className="styled-link" onClick={onLogout} to="/">로그아웃</Link></li>
             </>
           )}
         </ul>
@@ -64,6 +64,3 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
 };
 
 export default Nav;
-
-
-
