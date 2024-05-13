7e31e7b8c6973f77ac3710bc45184498dbd97d3c
dlwnajr <sh>
2024-05-13 18:21:52 +0900

    fix: 필터 서버 변경

--- src/api/filter.js
+++ src/api/filter.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-
-const BASE_URL = 'http://52.79.232.68:8080';
+const BASE_URL = 'http://3.35.115.71:8080';
 // const BASE_URL = 'http://localhost:8080';
 
 export const sendRequest = async (selectedAreas, selectedTourType, selectedCategory, selectedCategoryMiddle,selectedCategoryThird) => {
