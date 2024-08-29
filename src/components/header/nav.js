7e2d9b2d928b1bfe53afa3d825ca4c9dc362e2d1
dlwnajr <chulsaniii@gmail.com>
2024-08-29 22:56:45 +0900

    feat:마이페이지 메뉴 설정

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -4,7 +4,7 @@ import './nav.css';
 
 const Nav = ({ isLoggedIn, nickname, onLogout }) => {
   const [showDropdown, setShowDropdown] = useState(false);
-  const [showModal, setShowModal] = useState(false); // 모달 상태 추가
+  const [showModal, setShowModal] = useState(false);
 
   const handleMouseEnter = () => {
     setShowDropdown(true);
@@ -25,6 +25,8 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
   const handleLinkClick = (path) => {
     if (!isLoggedIn) {
       setShowModal(true); // 로그인 상태가 아니면 모달 열기
+    } else {
+      window.location.href = path; // 로그인 상태일 때 해당 경로로 이동
     }
   };
 
@@ -50,27 +52,31 @@ const Nav = ({ isLoggedIn, nickname, onLogout }) => {
           </li>
         </ul>
         <ul className="nav-list-2">
-          {!isLoggedIn && ( // Render login and signup links only if not logged in
+          {!isLoggedIn && (
             <>
+              {/* "마이페이지" 클릭 시 로그인 상태가 아니면 모달 열기 */}
+              <li className="nav-item"><Link className="styled-link-mypage" to="#" onClick={() => handleLinkClick('/mypage')}>마이페이지</Link></li>
               <li className="auth-item"><Link className="styled-link-login" to="/login">LOGIN</Link></li>
               <li className="auth-item"><Link className="styled-link-signup" to="/signup">SIGN UP</Link></li>
             </>
           )}
           {isLoggedIn && (
             <>
+              <li className="nav-item"><Link className="styled-link-mypage" to="/mypage">마이페이지</Link></li>
               <li className="nav-item">
                 <div className="styled-user">{nickname}님</div>
               </li>
+
               <li className="nav-item"><Link className="styled-link-logout" onClick={onLogout} to="/">LOGOUT</Link></li>
             </>
           )}
         </ul>
       </div>
 
-      {showModal && ( // 모달 표시
+      {showModal && (
         <div className="login-modal">
           <div className="login-modal-content">
-            <p>동선 추천은 로그인 후 이용이 가능합니다. <br/> 로그인하시겠습니까?</p>
+            <p>로그인 후 이용이 가능합니다. <br /> 로그인하시겠습니까?</p>
             <button onClick={() => { setShowModal(false); window.location.href = '/login'; }}>로그인</button>
             <button onClick={() => setShowModal(false)}>취소</button>
           </div>
